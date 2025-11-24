71071c1aae8b10fcffa8e4f5b9dcab3527464a7d
gumptao <tj22@tsinghua.org.cn>
2025-11-24 15:24:12 +0800

    fix: update torchopsloader

--- src/flagbench/dataset/dataloader.py
+++ src/flagbench/dataset/dataloader.py
@@ -2,6 +2,52 @@ import json
 import ijson
 from pathlib import Path
 from typing import Dict, List, Optional
+import torch
+from torch import FunctionSchema
+from torch._ops import _OpNamespace
+from dataclasses import dataclass
+
+@dataclass
+class APIInfo:
+    namespace: str
+    api: str
+    schemas: Dict[str, str]
+
+    def __post_init__(self):
+        if self.schemas and isinstance(next(iter(self.schemas.values())), FunctionSchema):
+            self.schemas = {k: str(v) for k, v in self.schemas.items()}
+
+class TorchOpsLoader:
+    def __init__(self):
+        self._cache = {}
+
+    def load_namespace(self, namespace: str) -> Dict[str, APIInfo]:
+        assert namespace in dir(torch.ops), f"Namespace {namespace} not found in torch.ops"
+        assert isinstance(getattr(torch.ops, namespace), _OpNamespace), f"{namespace} is not a valid OpNamespace"
+        if namespace in self._cache:
+            return self._cache[namespace]
+        ns_module = getattr(torch.ops, namespace)
+        ops_dict = {}
+        for op_name in dir(ns_module):
+            op = getattr(ns_module, op_name)
+            if callable(op):
+                # ops_dict[op_name] = {"schemas": op._schemas, "api": op.__module__ + "." + op.__name__}
+                ops_dict[op_name] = APIInfo(api=op.__module__ + "." + op.__name__, schemas=op._schemas, namespace=namespace)
+        self._cache[namespace] = ops_dict
+        return ops_dict
+    
+    def get_operator(self, namespace: str, op_name: str) -> Optional[APIInfo]:
+        ns_data = self.load_namespace(namespace)
+        return ns_data.get(op_name, None)
+    
+    def list_namespaces(self) -> List[str]:
+        return [ns for ns in dir(torch.ops) if isinstance(getattr(torch.ops, ns), _OpNamespace)]
+    
+    def load_all(self) -> Dict[str, Dict[str, APIInfo]]:
+        all_data = {}
+        for namespace in self.list_namespaces():
+            all_data[namespace] = self.load_namespace(namespace)
+        return all_data
 
 class OperatorLoader:
     """专门用于加载 all_operators.json 的工具类"""
@@ -48,16 +94,7 @@ class OperatorLoader:
 
 
 if __name__ == "__main__":
-    loader = OperatorLoader()
-    
-    # 方式1: 只加载需要的命名空间
+    loader = TorchOpsLoader()
     aten_ops = loader.load_namespace('aten')
-    print(f"Loaded {len(aten_ops)} aten operators")
-    
-    # 方式2: 获取特定算子
-    batch_norm_ops = loader.get_operator('aten', '_batch_norm_with_update')
-    print(f"Found {len(batch_norm_ops)} variants")
-    
-    # 方式3: 列出所有命名空间
-    namespaces = loader.list_namespaces()
-    print(f"Available namespaces: {namespaces}")
+    print(f"Loaded {len(aten_ops)} aten operators.")
+    print(aten_ops['abs'])
