dc434c3f2c5146132dc3efc771b6ca4292930ae3
gumptao <tj22@tsinghua.org.cn>
2025-12-19 16:11:15 +0800

    [dataset] add dataset import

--- src/flagbench/dataset/__init__.py
+++ src/flagbench/dataset/__init__.py
@@ -1,2 +1,3 @@
 from .kernel_list import PYTORCH_OPERATORS, IMPL_INFO, Autograd
+from .kernel_list import NON_FLAGGEMS_OPERATORS, V2_OPERATORS, V1_OPERATORS
 from .dataloader import OperatorLoader, TorchOpsLoader, APIInfo
